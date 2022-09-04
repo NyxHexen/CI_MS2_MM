9ca6fb2073fc94953f63f7d4f9e7f7a86ad5298f
NyxHexen <jd.davis948@gmail.com>
2022-09-04 19:35:22 +0100

    Add: re-added pathname condition for carousel removed by accident

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,48 +46,50 @@ modalContainer.addEventListener('click', () => {
 });
 
 // Carousel - index.html
-const carousel = document.getElementById('carousel');
-const carouselSlides = document.querySelectorAll('.carousel-slide');
-const carouselTabs = document.querySelectorAll('.tab');
+if (window.location.pathname === '/index.html') {
+    const carousel = document.getElementById('carousel');
+    const carouselSlides = document.querySelectorAll('.carousel-slide');
+    const carouselTabs = document.querySelectorAll('.tab');
 
-var carouselIndex = 0;
+    let carouselIndex = 0;
 
-const toggleActive = () => {
-    carouselTabs[carouselIndex].classList.toggle('active');
-    carouselSlides[carouselIndex].removeAttribute('style');
-}
+    const toggleActive = () => {
+        carouselTabs[carouselIndex].classList.toggle('active');
+        carouselSlides[carouselIndex].removeAttribute('style');
+    }
 
-let activeTimeout = setInterval(toggleActive, 2000);
+    let activeTimeout = setInterval(toggleActive, 2000);
 
-for (let i = 0; i < carouselTabs.length; i++) {
-    carouselTabs[i].addEventListener("click", () => {
-        for (let j = 0; j < carouselTabs.length; j++) {
-            carouselTabs[j].classList.remove('active');
-            carouselSlides[j].removeAttribute('style');
+    for (let i = 0; i < carouselTabs.length; i++) {
+        carouselTabs[i].addEventListener("click", () => {
+            for (let j = 0; j < carouselTabs.length; j++) {
+                carouselTabs[j].classList.remove('active');
+                carouselSlides[j].removeAttribute('style');
+            }
+            carouselIndex = i;
+            carouselTabs[carouselIndex].classList.toggle('active');
+            carouselSlides[carouselIndex].style.setProperty("z-index", "4");
+        })
+    }
+
+    const slideNext = () => {
+        if (carouselIndex === carouselSlides.length - 1) {
+            carouselIndex = 0;
+        } else {
+            carouselIndex++;
         }
-        carouselIndex = i;
         carouselTabs[carouselIndex].classList.toggle('active');
         carouselSlides[carouselIndex].style.setProperty("z-index", "4");
-    })
-}
-
-const slideNext = () => {
-    if (carouselIndex === carouselSlides.length - 1) {
-        carouselIndex = 0;
-    } else {
-        carouselIndex++;
     }
-    carouselTabs[carouselIndex].classList.toggle('active');
-    carouselSlides[carouselIndex].style.setProperty("z-index", "4");
-}
 
-// On hover stop the timers
-let carouselTimer = setInterval(slideNext, 2000);
-carousel.addEventListener('mouseenter', (e) => {
-    clearInterval(activeTimeout);
-    clearInterval(carouselTimer);
-});
-carousel.addEventListener('mouseleave', (e) => {
-    activeTimeout = setInterval(toggleActive, 2000);
-    carouselTimer = setInterval(slideNext, 2000);
-});
+    // On hover stop the timers
+    let carouselTimer = setInterval(slideNext, 2000);
+    carousel.addEventListener('mouseenter', (e) => {
+        clearInterval(activeTimeout);
+        clearInterval(carouselTimer);
+    });
+    carousel.addEventListener('mouseleave', (e) => {
+        activeTimeout = setInterval(toggleActive, 2000);
+        carouselTimer = setInterval(slideNext, 2000);
+    });
+}
