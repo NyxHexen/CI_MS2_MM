fd28bc365054a70085f7971ce5b2900156cc25aa
NyxHexen <jd.davis948@gmail.com>
2022-09-18 21:34:35 +0100

    Change: wrapped all code in DOMContentLoaded event listener as Jest was showing errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,26 @@
-// Hamburger Button
-document.querySelector('.hamburger-menu-button').addEventListener('click', function () {
-    document.querySelector('.nav-links-container').classList.toggle('active');
-});
-
-// Methodology - index.html
-const methods = document.querySelectorAll('.method');
-
-methods.forEach(method => {
-    method.addEventListener('click', () => {
-        method.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', () => {
+    // Hamburger Button
+    document.querySelector('.hamburger-menu-button').addEventListener('click', function () {
+        document.querySelector('.nav-links-container').classList.toggle('active');
     });
-});
 
-//Modal #Legal
-const modalContainer = document.querySelector('#modal-container');
-const modalHeading = document.querySelector('.modal').childNodes[1];
-const modalText = document.querySelector('.modal').childNodes[3];
-const legalButtons = document.querySelectorAll('.legal-button');
-const legalSwitch = [{
-    "h2": "TERMS OF USE",
-    "text": `
+    // Methodology - index.html
+    const methods = document.querySelectorAll('.method');
+
+    methods.forEach(method => {
+        method.addEventListener('click', () => {
+            method.classList.toggle('active');
+        });
+    });
+
+    //Modal #Legal
+    const modalContainer = document.querySelector('#modal-container');
+    const modalHeading = document.querySelector('.modal').childNodes[1];
+    const modalText = document.querySelector('.modal').childNodes[3];
+    const legalButtons = document.querySelectorAll('.legal-button');
+    const legalSwitch = [{
+        "h2": "TERMS OF USE",
+        "text": `
     <h3> 1. Right to Access </h3>
     <p> We grant you a limited, nonexclusive, nontransferable right to access this Site and its content for your
         personal, noncommercial use according to these Terms of Use.If you are under age 18, you may use this Site only
@@ -71,50 +72,41 @@ const legalSwitch = [{
         Privacy Policy, and which may be modified from time to time in our discretion, which modifications are effective
         as of the date posted on our Site.Your continued access or use of the Site or purchase or use of our Services
         indicates that you agree with such modifications.</p>`
-}, {
-    "h2": "PRIVACY POLICY",
-    "text": `
+    }, {
+        "h2": "PRIVACY POLICY",
+        "text": `
     <h3>What Information Does This Privacy Policy Cover?</h3>
     <br>
     <p>This Privacy Policy covers our treatment of personally identifiable information. Such  information may include name, mailing address, email address, telephone number, and other  information which identifies you as a specific individual ("Personal Information"). Please see  additional information below on the information we collect. For this Privacy Policy the definition  of “Personal Information” is the definition under the state, country, or other law applicable to the  person whose data is collected. For California residents only, “Personal Information” shall have  the definition as set forth in the California Consumer Privacy Act of 2018 (“CCPA”). Please see  the section below entitled “Privacy Notice for California Residents” for more information. If you  are a citizen or resident of the European Economic Area, United Kingdom, or Switzerland, the  definition of personal information ("personal data") is defined under GDPR and you have certain rights; therefore, please see the section below entitled "GDPR".</p>`
-}];
+    }];
 
-
-// Depending on which button is clicked (Terms & Conditions or Privacy Policy) the content of the modal changes.
-legalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (button.id === "tnc") {
-            modalHeading.innerHTML = legalSwitch[0].h2;
-            modalText.innerHTML = legalSwitch[0].text;
-            modalContainer.removeAttribute('class');
-            modalContainer.classList.toggle('active');
-        } else {
-            modalHeading.innerHTML = legalSwitch[1].h2;
-            modalText.innerHTML = legalSwitch[1].text;
-            modalContainer.removeAttribute('class');
-            modalContainer.classList.toggle('active');
-        }
+    // Depending on which button is clicked (Terms & Conditions or Privacy Policy) the content of the modal changes.
+    legalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (button.id === "tnc") {
+                modalHeading.innerHTML = legalSwitch[0].h2;
+                modalText.innerHTML = legalSwitch[0].text;
+                modalContainer.removeAttribute('class');
+                modalContainer.classList.toggle('active');
+            } else {
+                modalHeading.innerHTML = legalSwitch[1].h2;
+                modalText.innerHTML = legalSwitch[1].text;
+                modalContainer.removeAttribute('class');
+                modalContainer.classList.toggle('active');
+            }
+        });
     });
-});
 
-modalContainer.addEventListener('click', () => {
-    modalContainer.classList.toggle('out');
-});
+    modalContainer.addEventListener('click', () => {
+        modalContainer.classList.toggle('out');
+    });
 
-// Carousel - index.html
-const carousel = document.getElementById('carousel');
-const carouselSlides = document.querySelectorAll('.carousel-slide');
-const carouselTabs = document.querySelectorAll('.tab');
+    // Carousel - index.html
+    const carousel = document.getElementById('carousel');
+    const carouselSlides = document.querySelectorAll('.carousel-slide');
+    const carouselTabs = document.querySelectorAll('.tab');
 
-let carouselIndex = 0;
-
-// As index.js is used on each of the pages of the website we make sure that the below runs only on index.html
-if (window.location.pathname === '/CI_MS2_MM/index.html' || window.location.pathname === '/index.html') {
-    // https://www.byteblocks.com/Post/Use-addEventListener-or-attachEvent-for-windowonload-event
-    // Instead of reassigning the event handler we use addEvent to add to the event handlers chain.
-    window.addEventListener ?
-        window.addEventListener("load", nextSlide, false) :
-        window.attachEvent && window.attachEvent("onload", nextSlide);
+    let carouselIndex = 0;
 
     let carouselInterval = setInterval(nextSlide, 5000);
 
@@ -127,64 +119,71 @@ if (window.location.pathname === '/CI_MS2_MM/index.html' || window.location.path
     carousel.addEventListener('mouseleave', () => {
         carouselInterval = setInterval(nextSlide, 5000);
     });
-    
+
     // On tab click - focus slide
     carouselTabs.forEach(tab => {
         tab.addEventListener('click', selectedState);
     })
-}
 
-/**
- * nextSlide starts and progresses the carousel to the next tab and slide.
- * It first clears any active classes from the HTML elements using clearState func,
- * adds 1 to the carouselIndex variable and calls activeState func with the variable as a parameter.
- * This func takes no parameters.
- */
-function nextSlide() {
-    clearState();
-    if (carouselIndex === carouselSlides.length - 1) {
-        carouselIndex = 0;
-    } else {
-        carouselIndex++;
+    /**
+     * nextSlide starts and progresses the carousel to the next tab and slide.
+     * It first clears any active classes from the HTML elements using clearState func,
+     * adds 1 to the carouselIndex variable and calls activeState func with the variable as a parameter.
+     * This func takes no parameters.
+     */
+    function nextSlide() {
+        clearState();
+        if (carouselIndex === carouselSlides.length - 1) {
+            carouselIndex = 0;
+        } else {
+            carouselIndex++;
+        }
+        activeState(carouselIndex);
     }
-    activeState(carouselIndex);
-}
 
 
-/**
- * clearState cycles through each tab and slide and removes the 'active' class.
- * This function takes no parameters.
- */
-function clearState() {
-    carouselTabs.forEach(tab => {
-        tab.classList.remove('active');
-    })
-    carouselSlides.forEach(slide => {
-        slide.classList.remove('active');
-    })
-}
+    /**
+     * clearState cycles through each tab and slide and removes the 'active' class.
+     * This function takes no parameters.
+     */
+    function clearState() {
+        carouselTabs.forEach(tab => {
+            tab.classList.remove('active');
+        })
+        carouselSlides.forEach(slide => {
+            slide.classList.remove('active');
+        })
+    }
 
-/**
- * activeState takes an index and adds 'active' class to the corresponding HTML element.
- * @param {number} index - corresponds to the index of the carousel tabs and slides arrays.
- */
-function activeState(index) {
-    carouselTabs[index].classList.add('active');
-    carouselSlides[index].classList.add('active');
-}
+    /**
+     * activeState takes an index and adds 'active' class to the corresponding HTML element.
+     * @param {number} index - corresponds to the index of the carousel tabs and slides arrays.
+     */
+    function activeState(index) {
+        carouselTabs[index].classList.add('active');
+        carouselSlides[index].classList.add('active');
+    }
 
-/**
- * selectedState removes all classes from tabs and slides, uses a Regular Expression to 
- * find the index of the the event target inside the carousel tabs array, 
- * then calls activeState func using the index as parameter and reassigns the carouselIndex variable to the index.
- * @param {event} e - takes on click event as parameter.
- */
-function selectedState(e) {
-    clearState();
-    const selectedTab = e.target.classList.value;
-    // activeIndex is assigned to a RegExp to extract specific text from a string
-    // https://stackoverflow.com/questions/41515234/extract-a-specific-word-from-string-in-javascript
-    const activeIndex = selectedTab.match(/tab-(\d)/)[1];
-    activeState(activeIndex - 1);
-    carouselIndex = activeIndex - 1;
-}
+    /**
+     * selectedState removes all classes from tabs and slides, uses a Regular Expression to 
+     * find the index of the the event target inside the carousel tabs array, 
+     * then calls activeState func using the index as parameter and reassigns the carouselIndex variable to the index.
+     * @param {event} e - takes on click event as parameter.
+     */
+    function selectedState(e) {
+        clearState();
+        const selectedTab = e.target.classList.value;
+        // activeIndex is assigned to a RegExp to extract specific text from a string
+        // https://stackoverflow.com/questions/41515234/extract-a-specific-word-from-string-in-javascript
+        const activeIndex = selectedTab.match(/tab-(\d)/)[1];
+        activeState(activeIndex - 1);
+        carouselIndex = activeIndex - 1;
+    }
+
+    module.exports = {
+        nextSlide,
+        activeState,
+        clearState
+    };
+
+})
